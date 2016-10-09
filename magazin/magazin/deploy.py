62cb1c308d8c692ab83008562ce3a9df10babf09
gorbachevdmitry <dmgorbacheww@gmail.com>
2016-10-09 11:50:11 +0300

    add amazon s3 cool

--- magazin/magazin/deploy.py
+++ magazin/magazin/deploy.py
@@ -13,8 +13,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "magazin.deploy_settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
 
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
--- magazin/magazin/settings.py
+++ magazin/magazin/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = (
     'orders',
     'paypal.standard.ipn',
     'payment',
+    'storages',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -124,13 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assets/media/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -138,3 +139,33 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # django-paypal settings
 PAYPAL_RECEIVER_EMAIL = 'gorbachevdmitr@gmail.com'
 PAYPAL_TEST = True
+
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'Cache-Control': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'lavka'
+AWS_ACCESS_KEY_ID = 'AKIAJXU6TYZVHOIRF4YA'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+    # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+    # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+    # We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, ifyou're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
