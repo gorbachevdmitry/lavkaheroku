fb0769bf905380d3b61bf71321a839a53ea3bd24
gorbachevdmitry <dmgorbacheww@gmail.com>
2016-10-09 08:06:23 +0300

    add whitenoise

--- magazin/magazin/deploy.py
+++ magazin/magazin/deploy.py
@@ -13,6 +13,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "magazin.deploy_settings")
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
